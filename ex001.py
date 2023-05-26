7ca251b99c1184dcb947c4ec8cdc3558ef32cdda
gutierreab <122397588+gutierreab@users.noreply.github.com>
2023-05-26 17:11:20 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,7 @@
+# Programa para digitação de texto com fonte colorida.
+
+# Fonte: Cor Azul, sublinhado em Preto.
+
+print('\33[1;36;40mOlá, Mundo!\33[m')
+
+# Criado por Amanda Borges Gutierre, em 11 de Julho de 2022.
