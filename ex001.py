48d8d500eebd99fe5bbdaf09ae08c95e4b00e383
Amanda Borges Gutierre <122397588+gutierreab@users.noreply.github.com>
2023-07-30 08:58:32 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,7 +1,10 @@
-# Programa para digitação de texto com fonte colorida.
+# Programa para digitação de texto com fonte colorida no Python.
+# Printing colored text in Python.
 
 # Fonte: Cor Azul, sublinhado em Preto.
+# Font: Color Blue, underlined in Black.
 
 print('\33[1;36;40mOlá, Mundo!\33[m')
 
 # Criado por Amanda Borges Gutierre, em 11 de Julho de 2022.
+# Created by Amanda Borges Gutierre, em 11 de Julho de 2022.
