1be70a6ac712da0ccabccc0b49858b4e29374203
Amanda Borges Gutierre <122397588+gutierreab@users.noreply.github.com>
2023-07-30 08:59:50 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -4,7 +4,7 @@
 # Fonte: Cor Azul, sublinhado em Preto.
 # Font: Color Blue, underlined in Black.
 
-print('\33[1;36;40mOlá, Mundo!\33[m')
+print('\33[1;36;40mHello, World!\33[m')
 
 # Criado por Amanda Borges Gutierre, em 11 de Julho de 2022.
 # Created by Amanda Borges Gutierre, em 11 de Julho de 2022.
